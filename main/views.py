e488b072cafdd299e23daddbbdec2147f3a0d5b5
Stephen Ndele <stephenndele09346@gmail.com>
2021-04-02 23:09:59 +0300

    add home function

--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 
 
-
+def home (request):
+    return HttpResponse("We love coding",)
