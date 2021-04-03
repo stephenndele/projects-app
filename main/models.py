e80d3b82776f0414e38e67c475fdd6a729ccb50e
Stephen Ndele <stephenndele09346@gmail.com>
2021-04-03 10:22:46 +0300

    add Project Model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=250)
+    user = models.ForeignKey(User, on_delete=CASCADE)
+    link = models.URLField()
+    description = models.TextField(max_length=6000)
+    release_date = models.DateField()
+    averageRating = models.FloatField(default=0)
+    image = models.URLField(default=None, null=True)
+
+    def __unicode__(self):
+        return self.name
+
+
+    def __str__(self):
+        return self.name
