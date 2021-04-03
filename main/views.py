33e62f2609a366f4888214e10e4f936ea6f99387
Stephen Ndele <stephenndele09346@gmail.com>
2021-04-03 10:14:35 +0300

    add index file

--- main/views.py
+++ main/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render, HttpResponse
-
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import *
+from .forms import *
+from django.contrib.auth.decorators import login_required
+from django.db.models import Avg
 # Create your views here.
 
 
-def home (request):
-    return HttpResponse("We love coding",)
+def home(request):
+    query = request.GET.get('title')
+    allProjects = None
+    if query:
+        allProjects = Project.objects.filter(name__icontains=query)
+    else:
+
+        allProjects = Project.objects.all()
+    context = {
+        "projects": allProjects,
+    }
+
+    return render( request,'main/index.html', context)
