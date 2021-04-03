daff2659eea51563025dedefa2612889a457fb8b
Stephen Ndele <stephenndele09346@gmail.com>
2021-04-03 13:05:10 +0300

    add news letter form

--- main/forms.py
+++ main/forms.py
@@ -14,3 +14,7 @@ class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
         fields = ("comment", "design_rating", "usability_rating", "content_rating")
+
+class NewsLetterForm(forms.Form):
+    your_name = forms.CharField(label='First Name',max_length=30)
+    email = forms.EmailField(label='Email')
