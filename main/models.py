2e805b27ee4cc0f51ee08bdbd2960c7d672483d8
Stephen Ndele <stephenndele09346@gmail.com>
2021-04-04 12:30:53 +0300

    add profile model

--- main/models.py
+++ main/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save 
+from django.dispatch import receiver 
+
+# from tinymce.models import HTMLField
+
 # Create your models here.
 
 class Project(models.Model):
@@ -29,3 +34,24 @@ class Review(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+class Profile(models.Model):  
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.CharField(max_length=1000, blank=True, null=True)
+    image = models.ImageField(
+            null=True, 
+            blank=True, 
+            height_field='height_field', 
+            width_field='width_field'
+    )
+    projects = models.ManyToManyField(Project)
+
+    @receiver(post_save, sender=User) #add this
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User) #add this
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
