ec3ebca1fb7f8f68c759d360354b5d4167ea8b5c
Stephen Ndele <stephenndele09346@gmail.com>
2021-04-04 12:54:15 +0300

    add profile function

--- main/views.py
+++ main/views.py
@@ -159,6 +159,11 @@ def delete_review(request, project_id, review_id):
         return redirect("accounts:login")
 
 
+def userpage(request):
+	user_form = UserForm(instance=request.user)
+	profile_form = ProfileForm(instance=request.user.profile)
+	return render(request=request, template_name="main/user.html", context={"user":request.user, "user_form":user_form, "profile_form":profile_form })
+
 
 
 
