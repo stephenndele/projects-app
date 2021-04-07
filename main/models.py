399c13b22cd08dfe887da6efd03394cee3cf2a55
Stephen Ndele <stephenndele09346@gmail.com>
2021-04-07 08:33:24 +0300

    install restframework

--- main/models.py
+++ main/models.py
@@ -28,12 +28,12 @@ class Review(models.Model):
     project = models.ForeignKey('Project', on_delete=models.CASCADE, related_name="review")
     user = models.ForeignKey('Profile', on_delete=models.CASCADE, related_name="review")
     comment = models.TextField(max_length=1000, null=True, blank=True)
-    design_rating = models.FloatField(default=0, null=True, blank=True)
-    usability_rating = models.FloatField(default=0, null=True, blank=True)
-    content_rating = models.FloatField(default=0, null=True, blank=True)
+    design_rating = models.FloatField(null=True, blank=True)
+    usability_rating = models.FloatField(null=True, blank=True)
+    content_rating = models.FloatField(null=True, blank=True)
 
     def __str__(self):
-        return self.user.username
+        return f'{self.user.bio} Project'
 
 
 class Profile(models.Model):  
